4a6620ae4388b359acfd0dff9ad6c0fc19831774
code-bots-dev <codebotsdev@gmail.com>
2025-03-19 21:56:38 -0300

    Update main.py

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,3 +1,16 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/usuario/<usuario>")
+@login_required
+def usuario(usuario):
+    return render_template("usuario.html", usuario=usuario)
+
+@app.route("/pesquisaexperiencia")
+def pesquisa():
+    return render_template("pesquisaexper.html")
