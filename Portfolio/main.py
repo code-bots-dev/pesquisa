c43c5cf5816fc3804fc675298b8d0ccba862d8f6
code-bots-dev <codebotsdev@gmail.com>
2025-03-19 22:05:23 -0300

    Update main.py

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -13,6 +13,3 @@ def usuario(usuario):
 @app.route("/pesquisaexperiencia")
 def pesquisa():
     return render_template("pesquisaexper.html")
-
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=5001)  # Mudando a porta para 5001
