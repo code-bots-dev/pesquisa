8ea903afe2da82a2ba4dedd4d647d4cf41c41281
code-bots-dev <codebotsdev@gmail.com>
2025-03-19 22:03:26 -0300

    Update main.py

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,4 +14,5 @@ def usuario(usuario):
 def pesquisa():
     return render_template("pesquisaexper.html")
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, port=5001)  # Mudando a porta para 5001
