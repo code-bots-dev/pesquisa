504c90b8b64d6c44cfef5ed00b3a6bbb31a342bb
code-bots-dev <codebotsdev@gmail.com>
2025-03-19 22:08:47 -0300

    Update main.py

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,5 +1,8 @@
+import subprocess
+import multiprocessing
 from flask import Flask, render_template
 
+# Definindo o app Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,5 +17,23 @@ def usuario(usuario):
 def pesquisa():
     return render_template("pesquisaexper.html")
 
+# Função para rodar o Flask
+def run_flask():
+    app.run(debug=True, use_reloader=False, port=5001)  # Flask na porta 5001
+
+# Função para rodar o Streamlit
+def run_streamlit():
+    subprocess.run(["streamlit", "run", "main.py"])  # Assumindo que o arquivo principal do Streamlit é 'main.py'
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=5001)  # Mudando a porta para 5001
+    # Criando processos separados para Flask e Streamlit
+    flask_process = multiprocessing.Process(target=run_flask)
+    streamlit_process = multiprocessing.Process(target=run_streamlit)
+
+    # Inicia os dois processos
+    flask_process.start()
+    streamlit_process.start()
+
+    # Aguarda o término de ambos os processos
+    flask_process.join()
+    streamlit_process.join()
